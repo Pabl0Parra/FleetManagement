7c9bc68cd7797e01eba8a6385a50ef354dabc4ae
Pabl0Parra <pabsblue16@gmail.com>
2024-10-12 20:37:40 +0200

    modify Sidebar names

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -188,22 +188,6 @@ function Sidebar({ sidebarOpen, setSidebarOpen, variant = 'default' }) {
                           <li className="mb-1 last:mb-0">
                             <NavLink
                               end
-                              to="/"
-                              className={({ isActive }) =>
-                                'block transition duration-150 truncate ' +
-                                (isActive
-                                  ? 'text-violet-500'
-                                  : 'text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200')
-                              }
-                            >
-                              <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                Main
-                              </span>
-                            </NavLink>
-                          </li>
-                          <li className="mb-1 last:mb-0">
-                            <NavLink
-                              end
                               to="/table"
                               className={({ isActive }) =>
                                 'block transition duration-150 truncate ' +
@@ -217,6 +201,23 @@ function Sidebar({ sidebarOpen, setSidebarOpen, variant = 'default' }) {
                               </span>
                             </NavLink>
                           </li>
+                          <li className="mb-1 last:mb-0">
+                            <NavLink
+                              end
+                              to="/"
+                              className={({ isActive }) =>
+                                'block transition duration-150 truncate ' +
+                                (isActive
+                                  ? 'text-violet-500'
+                                  : 'text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200')
+                              }
+                            >
+                              <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                                Buoy Details
+                              </span>
+                            </NavLink>
+                          </li>
+
                           <li className="mb-1 last:mb-0">
                             <NavLink
                               end
